ab9721a3eba5aeb371e34b898be11281d015e1b9
gyuhyeun <ggm.s220079@ggh.goe.go.kr>
2022-08-18 09:01:13 +0900

    도윤이가 내준 숙제
    코드하나하나를 이해하려고 노력하면서 쓰긴했는데 외우고 쓴거같기도해서 다시한번 보고 이해할거다.

--- Assets/02_Scripts/Cookware/Fryer.cs
+++ Assets/02_Scripts/Cookware/Fryer.cs
@@ -6,11 +6,22 @@ public class Fryer : Cookware
 {
     public override void Cook(InteractionItem CookItem)
     {
-
+        item = CookItem;
+        StartCoroutine("Cooking");
     }
 
     public override IEnumerator Cooking()
     {
-        yield return null;
+        item.IsCooking = true;
+        yield return new WaitUntil(() => Input.GetKeyDown(KeyCode.LeftControl));
+        for(int i = 0; i < cookedItemList.Count; i++)
+        {
+            if(cookedItemList[i].name == $"Cooked{item.name}")
+            {
+                yield return new WaitForSeconds(cookTime);
+                item.GetComponent<SpriteRenderer>().sprite = cookedItemList[i].GetComponent<SpriteRenderer>().sprite;
+                item.IsCooking = false;
+            }
+        }
     }
 }
